6ae48d0eaec678d672f4a9355644af24d46f5508
WhisperVPN LTD <whispergroup@protonmail.com>
2022-05-09 21:04:32 +0200

    Create WhisperVPN.sh

---
+++ WhisperVPN.sh
@@ -0,0 +1,13 @@
+red="\e[34m"
+rset="\e[34m"
+grn="\e[34m"
+ylo="\e[34m"
+blue="\e[34m"
+cyan="\e[34m"
+pink="\e[34m"
+#-----------------
+# scripting start
+banner(){
+vid
+echo -e '\e[34m                                                                                                                                                                                                                                                                                                                  
+   
