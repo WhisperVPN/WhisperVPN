e6b7c94c78866fab472e5ab8d36be98ccedef942
WhisperVPN LTD <whispergroup@protonmail.com>
2022-05-09 22:05:58 +0200

    Update WhisperVPN.sh

--- WhisperVPN.sh
+++ WhisperVPN.sh
@@ -74,7 +74,72 @@ elif [ $optnz = "5" ];
 then
 exit 1
 else
-echo "ERROR000 - Sometingh went wrong :/"
+echo "ERROR000 - Something went wrong :/"
 exit
 fi
 }
+upd(){
+if [ -d "$HOME/WhisperVPN" ];
+then
+cd $HOME
+rm -rf WhisperVPN
+elif [ -d "$HOME/WhisperVPN" ];
+then
+cd $HOME
+rm -rf WhisperVPN
+else
+echo
+exit 1
+fi
+cd $HOME
+sleep 1
+echo -e "         \e[34mUpdating WhisperVPN...\e[34m"
+echo
+printf "                     \e[34m["
+# While process is running...
+while git clone https://github.com/ParzivalHack/WhisperVPN 2> /dev/null; do 
+    printf  "\e[34m▓▓▓▓▓▓▓▓▓▓▓▓▓\e[34m"
+    sleep 1
+done
+printf "\e[34m]\e[34m"
+echo
+echo
+echo
+printf "\e[34m           Updated successfully to the latest version :D\e[34m"
+sleep 2.0
+clear
+cd $HOME
+cd WhisperVPN
+bash WhisperVPN.sh
+}
+vid(){
+FILE=$HOME/WhisperVPN
+if [ -f "$FILE" ]; then
+pop
+else
+echo
+fi
+}
+pop(){
+clear
+read p
+if [ "$p" = "y" ];
+then
+am start -a android.intent.action.VIEW -d ERROR000 2>/dev/null
+clear
+menu
+elif [ "$p" = "t" ];
+then
+clear
+menu
+elif [ "$p" = "z" ];
+then
+cd $HOM/doomshell
+rm noob.noob
+menu
+else
+menu
+exit
+fi
+}
+menu
