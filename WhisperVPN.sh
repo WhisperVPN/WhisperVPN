7764c8396d81a7c59246374892206da15324d2a9
WhisperVPN LTD <whispergroup@protonmail.com>
2022-05-09 21:59:40 +0200

    Update WhisperVPN.sh

--- WhisperVPN.sh
+++ WhisperVPN.sh
@@ -3,11 +3,11 @@ rset="\e[34m"
 grn="\e[34m"
 ylo="\e[34m"
 blue="\e[34m"
-cyan="\e[34m"
+cyan="\e[36m"
 pink="\e[34m"
 #-----------------
 # scripting start
-banner(){
+menu(){
 vid
 echo -e '\e[34m                                     ..::...                                             
                                             .^~!7777777!^.                                          
@@ -34,6 +34,47 @@ echo -e '\e[34m                                     ..::...
                                          ^J?^...^7YY?:              .. .^!!!!!!!!~:                 
                                           .!?JJJJJ?~.                     ......                   
                                              .:::.  
-                                                   © 2022 WhisperVPN LTD.
-Hhh                                                                                                                                                                                                                                                                                                                                                              
+echo -e '\e[36m                                    © 2022 WhisperVPN LTD
+echo " "
+echo " "
+}
+menu(){
+echo -e "$cyan             ＞＞＞＞>>>＞$cyan [Menu]$grn ＜<<<＜＜＜＜$cyan"
+echo " "
+echo " "
+echo -e "$red                        ➡$cyan [\e[36m1\e[36m] Start VPN"
+echo -e "$red                        ➡$cyan [\e[36m2\e[36m] About Us"
+echo -e "$red                        ➡$cyan [\e[36m3\e[36m] Update Script"
+echo -e "$red                        ➡$cyan [\e[36m4\e[36m] Report Issues"
+echo -e "$red                        ➡$cyan [\e[36m5\e[36m] Exit"
    
+echo " "
+echo " "
+echo -e "$grn               >＞＞＞＞＞$cyan [SELECT]$cyan ＜＜＜＜＜<$rset"
+echo " "
+echo -ne "\e[36m#SELECT OPTION\e[36m: "
+read optnz
+if [ $optnz = "1" ];
+then
+cd $HOME
+cd WhisperVPN
+tor
+elif [ $optnz = "2" ];
+then
+am start -a android.intent.action.VIEW -d https://whispervpn.company.site/ > /dev/null 2>&1
+menu
+elif [ $optnz = "3" ];
+then
+upd
+elif [ $optnz = "4" ];
+then
+am start -a android.intent.action.VIEW -d https://github.com/WhisperVPN/WhisperVPN/issues > /dev/null 2>&1
+menu
+elif [ $optnz = "5" ];
+then
+exit 1
+else
+echo "ERROR000 - Sometingh went wrong :/"
+exit
+fi
+}
