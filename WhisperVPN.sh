c22e9e1d20249d40de2567f1798c58f0c6d792d8
WhisperVPN LTD <whispergroup@protonmail.com>
2022-05-09 23:01:44 +0200

    Update WhisperVPN.sh

--- WhisperVPN.sh
+++ WhisperVPN.sh
@@ -24,7 +24,7 @@ echo " "
 echo " "
 }
 menu(){
-echo -e "$cyan             ＞＞＞＞>>>＞$cyan © 2022 WhisperVPN LTD. $cyan ＜<<<＜＜＜＜$cyan"
+echo -e "                $pink © 2022 WhisperVPN LTD. $pink"
 echo -e "$cyan             ＞＞＞＞>>>＞$cyan [Menu]$cyan ＜<<<＜＜＜＜$cyan"
 echo " "
 echo " "
